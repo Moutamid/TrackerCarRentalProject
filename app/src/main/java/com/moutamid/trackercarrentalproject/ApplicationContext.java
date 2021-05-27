3e43029d737a7c9ba0b9cc20625bcd3739d2f49b
Moutamid <lumianokia188@gmail.com>
2021-05-27 10:29:38 +0500

    27 05 2021

---
+++ app/src/main/java/com/moutamid/trackercarrentalproject/ApplicationContext.java
@@ -0,0 +1,60 @@
+package com.moutamid.trackercarrentalproject;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ApplicationContext extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.child("requests")
+                .child(auth.getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (!snapshot.exists())
+                            return;
+
+                        if (!snapshot.child("pushKey").exists())
+                            return;
+
+                        String keyStr = snapshot.child("pushKey").getValue(String.class);
+
+                        if (snapshot.child("status").exists()) {
+                            String statusStr = snapshot.child("status").getValue(String.class);
+
+                            databaseReference.child("booking_history").child(keyStr)
+                                    .child("status").setValue(statusStr);
+                        }
+
+                        if (snapshot.child("currentMileages").exists()) {
+                            Boolean currentMileagesStr = snapshot.child("currentMileages").getValue(Boolean.class);
+
+                            databaseReference.child("booking_history").child(keyStr)
+                                    .child("currentMileages")
+                                    .setValue(currentMileagesStr);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+    }
+}
